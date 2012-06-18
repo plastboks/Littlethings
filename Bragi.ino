e0eb7e12a2560b5d2789a53c9e9d0075488ee9eb
Alexander Skjolden <alex@plastboks.net>
2012-06-18 20:21:59 +0200

    Big bugfixes

--- Bragi.ino
+++ Bragi.ino
@@ -3,15 +3,12 @@
 #include <EEPROM.h>
 #include <stdarg.h>
 
-
 TinyGPS gps;
 U8GLIB_ST7920_128X64 u8g(9, 8, 17, U8G_PIN_NONE); 
-
 
 // Hardware pins.
 const int ledPin = 13;
 const int buttonPin = 5;
-
 
 // Global variables.
 int neverHadFix = 1;
@@ -20,12 +17,15 @@ bool newData = false;
 long previousMillis = 0;
 long interval = 500;
 int switchTemplate = EEPROM.read(1);
+float flat, flon, fkmph, falt, fc;
+unsigned long age, fix_age, speed;
+int year, satellites, hdop;
+byte month, day, hour, minutes, second, hundredths;
 float lastLat = 0.0;
 float lastLon = 0.0;
 float aDriven = 0.0;
 float bDriven = 0.0;
 static FILE lcdout = {0} ;  // LCD FILE structure
-
 
 // LCD character writer
 static int lcd_putchar(char ch, FILE* stream) {
@@ -33,9 +33,7 @@ static int lcd_putchar(char ch, FILE* stream) {
   return (0) ;
 }
 
-
 void setup() {
-  // set serial speeds.
   Serial.begin(57600); 
   // setup digital pins.
   pinMode(ledPin, OUTPUT);
@@ -45,36 +43,15 @@ void setup() {
 }
 
 
-void gpsData(void) {
-  
-  float flat, flon, fkmph, falt, fc;
-  unsigned long age, fix_age, speed;
-  int year, satellites, hdop;
-  byte month, day, hour, minutes, second, hundredths;
-  
-  // reset faultCounter.
+bool gpsGetData(void) {
   faultCounter ++;
-  
-  // check the serialdata 10 times.
-  for (unsigned long start = millis(); millis() - start < 10;) {
-    if (Serial.available()) {
-      char c = Serial.read();
-      if (gps.encode(c)) {
-        // deactivate faulcounter
+  for (unsigned long start = millis(); millis() - start < 500;) {
+    while (Serial.available()) {
+      if (gps.encode(Serial.read()))
         faultCounter = 0;
-      }
     }
   }
-  
-  // check if faultcounter has exceeded 50 turns.
   if (faultCounter < 50) {
-     newData = true;
-  } else {
-     newData = false; 
-  }
-  
-  if (newData) {
-
     gps.f_get_position(&flat, &flon, &age);
     fkmph = gps.f_speed_kmph();
     falt = gps.f_altitude();
@@ -83,11 +60,22 @@ void gpsData(void) {
     satellites = gps.satellites();
     hdop = gps.hdop();    
     gps.crack_datetime(&year, &month, &day, &hour, &minutes, &second, &hundredths, &fix_age);
+    return true;
+  } else {
+    return false; 
+  }
+}
 
-    if (neverHadFix == 1) { lastLat = flat; lastLon = flon; }
-    if (fkmph > 1.0) { aDriven = aDriven + gps.distance_between(flat, flon, lastLat, lastLon); }
-   
-    // graphic commands to redraw the complete screen should be placed here  
+void noSignalGpsScreen(void) {
+  u8g.setFont(u8g_font_unifont);
+  u8g.drawStr(4, 22, "Wait for it...");
+}
+
+
+void firstGpsScreen(void) {
+  
+    //if (neverHadFix == 1) { lastLat = flat; lastLon = flon; }
+    //if (fkmph > 1.0) { aDriven = aDriven + gps.distance_between(flat, flon, lastLat, lastLon); }
     u8g.setFont(u8g_font_5x7);
     u8g.drawStr( 0, 6, "LA");
     u8g.setPrintPos(14, 6);
@@ -120,33 +108,25 @@ void gpsData(void) {
   
     u8g.setFont(u8g_font_5x7);
     u8g.setPrintPos(44, 52);
-    fprintf(&lcdout, "A: %dkm | B: %dkm", aDriven/1000, bDriven);
+    //fprintf(&lcdout, "A: %dkm | B: %dkm", aDriven/1000, bDriven);
     
     u8g.drawLine(0, 56, 128, 56);
     u8g.setFont(u8g_font_5x7);
     u8g.setPrintPos(14, 64);
     fprintf(&lcdout, "%d-%02d-%02d  %02d:%02d:%02d", year, month, day, hour, minutes, second);
-    
-    neverHadFix = 0;
-  } else {
-    u8g.setFont(u8g_font_unifont);
-    u8g.drawStr(4, 22, "Wait for it...");
-  }
-
 }
 
 
 // program loop
-void loop(void) {
-  
-  unsigned long currentMillis = millis();
-  if (millis() - previousMillis > interval) {
-    previousMillis = currentMillis;
-    // picture loop
-    u8g.firstPage();  
+void loop(void) {  
+  u8g.firstPage();  
+  if (gpsGetData()) {
     do {
-      gpsData();
-    } while( u8g.nextPage() );
+      firstGpsScreen();
+    } while(u8g.nextPage());
+  } else {
+    do {
+      noSignalGpsScreen();
+    } while(u8g.nextPage());
   }
-  
 }
