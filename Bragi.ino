757accd7b41b307af178358d1368e9347d1e6bd3
Alexander Skjolden <alex@plastboks.net>
2012-08-05 15:00:45 +0200

    shutdown and multiple screens

--- Bragi.ino
+++ Bragi.ino
@@ -9,9 +9,12 @@ U8GLIB_ST7920_128X64 u8g(9, 10, 17, U8G_PIN_NONE);
 // Hardware pins.
 const int ledPin = 13;
 const int buttonPin = 5;
+const int mainsPower = 8;
 
 // Global variables.
+uint8_t draw_state = 0;
 int neverHadFix = 1;
+int shutdownState = 0;
 long faultCounter = 50;
 bool newData = false;
 long previousMillis = 0;
@@ -50,6 +53,11 @@ double EEPROM_readDouble(int ee) {
   return value;
 } 
 
+void averageSpeed(void) {
+  i++;
+  avgspeedH = fkmph + avgspeedH;
+  avgspeed = avgspeedH / i;
+}
 
 bool gpsGetData(void) {
   faultCounter ++;
@@ -68,9 +76,34 @@ bool gpsGetData(void) {
     satellites = gps.satellites();
     hdop = gps.hdop();    
     gps.crack_datetime(&year, &month, &day, &hour, &minutes, &second, &hundredths, &fix_age);
+    averageSpeed();
     return true;
   } else {
     return false; 
+  }
+}
+
+
+void drawRotator(void) {
+  switch(draw_state >> 7) {
+    case 0: firstGpsScreen(); break;
+    case 1: secondGpsScreen(); break;
+  }
+}
+
+
+void powerLostScreen(void) {
+  if (shutdownState == 0) {
+    EEPROM_writeDouble(10, bDriven);
+    
+    shutdownState = 1;
+    u8g.setFont(u8g_font_10x20);
+    u8g.drawStr(10, 28, "Writing");
+    u8g.drawStr(48, 48, "...");
+  } else {
+    u8g.setFont(u8g_font_10x20);
+    u8g.drawStr(10, 28, "Shutdown");
+    u8g.drawStr(48, 48, "...");
   }
 }
 
@@ -83,14 +116,10 @@ void noSignalGpsScreen(void) {
 
 
 void firstGpsScreen(void) {
-  i++;
-  avgspeedH = fkmph + avgspeedH;
-  avgspeed = avgspeedH / i;
   
   if (neverHadFix == 1) { lastLat = flat; lastLon = flon; }
   if (fkmph > 1.0) { aDriven = aDriven + gps.distance_between(flat, flon, lastLat, lastLon); }
   if (fkmph > 1.0) { bDriven = bDriven + gps.distance_between(flat, flon, lastLat, lastLon); }
-  //if (bDriven > lastBDriven) { EEPROM_writeDouble(10, bDriven); }
   
   int timeZone = hour + 2;
   if (timeZone == 24) { timeZone = 0; }
@@ -100,81 +129,105 @@ void firstGpsScreen(void) {
   lastBDriven = bDriven;   
 
   u8g.drawLine(0, 8, 128, 8);
-  u8g.drawLine(0, 54, 128, 54);
-  u8g.drawLine(64, 56, 64, 64);
+  //u8g.drawLine(0, 54, 128, 54);
   
   u8g.setFont(u8g_font_6x12);
   u8g.setPrintPos(4, 7);
   fprintf(&lcdout, "%d-%02d-%02d  %02d:%02d:%02d", year, month, day, timeZone, minutes, second);
 
   u8g.setFont(u8g_font_10x20);
-  u8g.setPrintPos(32, 28);
+  u8g.setPrintPos(6, 28);
   u8g.print(fkmph, 1);
-  u8g.setFont(u8g_font_6x12);
-  u8g.setPrintPos(96, 28);
-  u8g.print("km/h");
+  u8g.setPrintPos(58, 28);
+  u8g.print("/");
+  u8g.setPrintPos(70, 28);
+  u8g.print(avgspeed, 1);
 
   u8g.setFont(u8g_font_6x12);
-  u8g.setPrintPos(12, 42);
+  u8g.setPrintPos(4, 54);
   u8g.print("A: ");
-  u8g.setPrintPos(26, 42);
+  u8g.setPrintPos(18, 54);
   u8g.print(aDriven/1000, 1);
-  u8g.setPrintPos(70, 42);
-  u8g.print("km");
 
-  u8g.setPrintPos(12, 52);
+  u8g.setPrintPos(4, 64);
   u8g.print("B: ");
-  u8g.setPrintPos(26, 52);
+  u8g.setPrintPos(18, 64);
   u8g.print(bDriven/1000, 1);
-  u8g.setPrintPos(70, 52);
-  u8g.print("km");
+
+  u8g.setPrintPos(64, 54);
+  u8g.print("C: ");
   
-  u8g.setFont(u8g_font_6x12);
-  u8g.setPrintPos(8, 64);
-  u8g.print(falt, 1);
-  u8g.setPrintPos(48, 64);
-  u8g.print("m");
-  u8g.setPrintPos(80, 64);
-  u8g.print(fc, 1);
-  u8g.setFont(u8g_font_5x7);
-  u8g.setPrintPos(112, 60);
-  u8g.print("o");
+  u8g.setPrintPos(64, 64);
+  u8g.print("D: ");  
   
   neverHadFix = 0;
 }
 
 
 void secondGpsScreen(void) {
-    u8g.setFont(u8g_font_6x12);
-    u8g.drawStr( 0, 6, "LA");
-    u8g.setPrintPos(14, 6);
-    u8g.print(flat, 6);
-    u8g.drawStr( 66, 6, "LO");
-    u8g.setPrintPos(80, 6);
-    u8g.print(flon, 6);
+  u8g.setFont(u8g_font_10x20);
+  u8g.drawStr(4, 16, "LAT");
+  u8g.setPrintPos(38, 16);
+  u8g.print(flat, 5);
+  u8g.drawStr(4, 32, "LON");
+  u8g.setPrintPos(38, 32);
+  u8g.print(flon, 5);
+  
+  u8g.setFont(u8g_font_6x12);
+  u8g.drawStr(4, 46, "ALT");
+  u8g.setPrintPos(24, 46);
+  u8g.print(falt, 1);
+  u8g.drawStr(64, 46, "COUR");
+  u8g.setPrintPos(90, 46);
+  u8g.print(fc, 1);
+
+  u8g.drawStr(4, 60, "SAT");
+  u8g.setPrintPos(24, 60);
+  u8g.print(satellites);
+  u8g.drawStr(64, 60, "HDOP");
+  u8g.setPrintPos(90, 60);
+  u8g.print(hdop);
 }
 
 
 void setup() {
-  //bDriven = EEPROM_readDouble(10);
+  bDriven = EEPROM_readDouble(10);
   Serial.begin(57600); 
   // setup digital pins.
   pinMode(ledPin, OUTPUT);
   pinMode(buttonPin, INPUT);
+  pinMode(mainsPower, INPUT);
   // fill in the LCD FILE structure
   fdev_setup_stream (&lcdout, lcd_putchar, NULL, _FDEV_SETUP_WRITE);
 }
 
 
 void loop(void) {
-  u8g.firstPage();
-  if (gpsGetData()) {
+  if (digitalRead(mainsPower) == HIGH) {
+    shutdownState = 0;
+    u8g.firstPage();
+    if (gpsGetData()) {
+      do {
+        drawRotator();
+      } while(u8g.nextPage());
+    } else {
+      do {
+        noSignalGpsScreen();
+      } while(u8g.nextPage());
+    }
+  }
+  if (digitalRead(mainsPower) == LOW) {
+    u8g.firstPage();
     do {
-      firstGpsScreen();
-    } while(u8g.nextPage());
-  } else {
-    do {
-      noSignalGpsScreen();
+      powerLostScreen();
     } while(u8g.nextPage());
   }
+  
+  draw_state++;
+  if ( draw_state >= 32*8 ) {
+    draw_state = 0;
+  }
+  
+  delay(50);
+
 }
