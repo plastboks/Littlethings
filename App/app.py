88f169c88a36c0e40bc206ace2af8e56b432a4da
Alexander Skjolden <alex@plastboks.net>
2012-09-11 17:45:04 +0200

    Added StaticText example

--- App/app.py
+++ App/app.py
@@ -15,6 +15,7 @@
 
 import sys
 import wx
+import textwrap
 
 #import Tweeno
 import config
@@ -24,6 +25,7 @@ class App(wx.Frame):
   def __init__(self, *args, **kwargs):
     super(App, self).__init__(*args, **kwargs)
     self.c = config.Config()
+
 
     self.InitUI()
 
@@ -40,14 +42,34 @@ class App(wx.Frame):
     menuBar.Append(self.fileMenu, "File")
     self.SetMenuBar(menuBar)
 
+    
+    pnl = wx.Panel(self)
+    vbox = wx.BoxSizer(wx.VERTICAL)
+    vbox.Add((-1, 15))
+
+    for count in range(1, 20):
+
+      tmp1 = "WordPress hack: Easily get post content by ID http://t.co/0mpCWRxc"
+      for line in textwrap.wrap(tmp1, 45):
+        vbox.Add(wx.StaticText(pnl, label=line, style=wx.ALIGN_LEFT), flag=wx.LEFT, border=10)
+      vbox.Add(wx.StaticLine(pnl, -1, size=(1,2), style=wx.LI_VERTICAL), flag=wx.EXPAND|wx.ALL, border=10)
+
+    pnl.SetSizer(vbox)
+
     self.Bind(wx.EVT_TOOL, self.onConfig, configFileMenu)
     self.Bind(wx.EVT_TOOL, self.onClose, closeFileMenu)
 
+    #self.scroll = wx.ScrolledWindow(self, -1)
+    #self.scroll.SetScrollbars(1, 1, 600, 400)
 
     self.SetSize((300, 700))
+    self.SetMaxSize((300, 700))
+    self.SetMinSize((300, 700))
     self.SetTitle('Tweeno')
     self.Centre()
     self.Show(True)
+
+
 
 
   def onConfig(self, event): 
