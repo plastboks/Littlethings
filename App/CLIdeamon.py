72d601ad2b9e188947456d2d1a7031688c2eaea9
Alexander Skjolden <alex@plastboks.net>
2012-09-17 17:10:21 +0200

    various tests

--- App/CLIdeamon.py
+++ App/CLIdeamon.py
@@ -34,14 +34,16 @@ d = LCD.p160_128(c.s_port, 57600)
 oldMessage = None
 
 while True:
-  statuses = t.GetFriendsTimeline(user="skjoldenfrilans", count=1)
-  message = [s.text for s in statuses][0].encode("utf-8", "ignore")
+  statuses = t.GetFriendsTimeline(user="skjoldenfrilans", count=1, retweets=True)
+  message = [s.text for s in statuses][0].encode("utf-8", "replace")
   user = [s.user.screen_name for s in statuses]
 
   if not message == oldMessage:
+    #print message + "   By: @" + user[0]
     print functions.replaceUTF(message) + "   @" + user[0]
     oldMessage = message
     d.clear()
+    #d.send(message)
     d.send(functions.replaceUTF(message))
     d.byline(user[0])
     d.end()
