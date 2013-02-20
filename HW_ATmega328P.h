ec64952008cc9f46a55e41f4107ccdc4306e7867
Alexander Skjolden <alex@plastboks.net>
2013-02-20 14:09:43 +0100

    stock 2.0 code

--- HW_ATmega328P.h
+++ HW_ATmega328P.h
@@ -1,4 +1,8 @@
 // *** Hardwarespecific functions ***
+void UTFT::_hw_special_init()
+{
+}
+
 void UTFT::LCD_Writ_Bus(char VH,char VL, byte mode)
 {   
 	switch (mode)
@@ -62,54 +66,24 @@ void UTFT::LCD_Writ_Bus(char VH,char VL, byte mode)
 		pulse_low(P_SCL, B_SCL);
 		break;
 	case 8:
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-		PORTA = VH;
-		pulse_low(P_WR, B_WR);
-		PORTA = VL;
-		pulse_low(P_WR, B_WR);
-#else
 		PORTD = VH;
 		pulse_low(P_WR, B_WR);
 		PORTD = VL;
 		pulse_low(P_WR, B_WR);
-#endif
 		break;
 	case 16:
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-		PORTA = VH;
-		PORTC = VL;
-#else
 		PORTD = VH;
 		cport(PORTC, 0xFC);
 		sport(PORTC, (VL>>6) & 0x03);
 		PORTB =  VL & 0x3F;
-#endif
 		pulse_low(P_WR, B_WR);
 		break;
 	case LATCHED_16:
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-		PORTG &= ~0x20;	
-		PORTG |= (VH & 0x10)<<1;	
-		PORTH &= ~0x18;	
-		PORTH |= (VH & 0xC0)>>3;	
-		PORTE &= ~0x3B;	
-		PORTE |= (VH & 0x03) + ((VH & 0x0C)<<2) + ((VH & 0x20)>>2);
-		cbi(P_ALE, B_ALE);
-		pulse_high(P_ALE, B_ALE);
-		cbi(P_CS, B_CS);
-		PORTG &= ~0x20;	
-		PORTG |= (VL & 0x10)<<1;	
-		PORTH &= ~0x18;	
-		PORTH |= (VL & 0xC0)>>3;	
-		PORTE &= ~0x3B;	
-		PORTE |= (VL & 0x03) + ((VL & 0x0C)<<2) + ((VL & 0x20)>>2);
-#else
 		PORTD = VH;
 		cbi(P_ALE, B_ALE);
 		pulse_high(P_ALE, B_ALE);
 		cbi(P_CS, B_CS);
 		PORTD =  VL;
-#endif
 		pulse_low(P_WR, B_WR);
 		sbi(P_CS, B_CS);
 		break;
@@ -118,27 +92,12 @@ void UTFT::LCD_Writ_Bus(char VH,char VL, byte mode)
 
 void UTFT::_set_direction_registers(byte mode)
 {
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	if (mode!=LATCHED_16)
-	{
-		DDRA = 0xFF;
-		if (mode==16)
-			DDRC = 0xFF;
-	}
-	else
-	{
-		DDRH = 0x18;
-		DDRG = 0x20;
-		DDRE = 0x3B;
-	}
-#else
 	DDRD = 0xFF;
 	if (mode==16)
 	{
 		DDRB |= 0x3F;
 		DDRC |= 0x03;
 	}
-#endif
 
 }
 
@@ -146,15 +105,10 @@ void UTFT::_fast_fill_16(int ch, int cl, long pix)
 {
 	long blocks;
 
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	PORTA = ch;
-	PORTC = cl;
-#else
 	PORTD = ch;
 	cport(PORTC, 0xFC);
 	sport(PORTC, (cl>>6) & 0x03);
 	PORTB =  cl & 0x3F;
-#endif
 
 	blocks = pix/16;
 	for (int i=0; i<blocks; i++)
@@ -187,11 +141,7 @@ void UTFT::_fast_fill_8(int ch, long pix)
 {
 	long blocks;
 
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	PORTA = ch;
-#else
 	PORTD = ch;
-#endif
 
 	blocks = pix/16;
 	for (int i=0; i<blocks; i++)
--- HW_ATmega32U4.h
+++ HW_ATmega32U4.h
@@ -1,4 +1,8 @@
 // *** Hardwarespecific functions ***
+void UTFT::_hw_special_init()
+{
+}
+
 void UTFT::LCD_Writ_Bus(char VH,char VL, byte mode)
 {   
 	switch (mode)
@@ -62,54 +66,53 @@ void UTFT::LCD_Writ_Bus(char VH,char VL, byte mode)
 		pulse_low(P_SCL, B_SCL);
 		break;
 	case 8:
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-		PORTA = VH;
+		cport(PORTC, 0xBF);
+		cport(PORTD, 0x60);
+		cport(PORTE, 0xBF);
+		PORTC += ((VH & 0x20)<<1);
+		PORTD += ((VH & 0x40)<<1) + (VH & 0x10) + ((VH & 0x08)>>3) + ((VH & 0x04)>>1) + ((VH & 0x03)<<2);
+		PORTE += ((VH & 0x80)>>1);
 		pulse_low(P_WR, B_WR);
-		PORTA = VL;
+
+		cport(PORTC, 0xBF);
+		cport(PORTD, 0x60);
+		cport(PORTE, 0xBF);
+		PORTC += ((VL & 0x20)<<1);
+		PORTD += ((VL & 0x40)<<1) + (VL & 0x10) + ((VL & 0x08)>>3) + ((VL & 0x04)>>1) + ((VL & 0x03)<<2);
+		PORTE += ((VL & 0x80)>>1);
 		pulse_low(P_WR, B_WR);
-#else
-		PORTD = VH;
-		pulse_low(P_WR, B_WR);
-		PORTD = VL;
-		pulse_low(P_WR, B_WR);
-#endif
 		break;
 	case 16:
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-		PORTA = VH;
-		PORTC = VL;
-#else
-		PORTD = VH;
-		cport(PORTC, 0xFC);
-		sport(PORTC, (VL>>6) & 0x03);
-		PORTB =  VL & 0x3F;
-#endif
+		cport(PORTB, 0x0F);
+		cport(PORTC, 0x3F);
+		cport(PORTD, 0x20);
+		cport(PORTE, 0xBF);
+		cport(PORTF, 0x3F);
+
+		PORTB |= ((VL & 0x0F)<<4);
+		PORTC |= ((VL & 0x20)<<2) + ((VH & 0x20)<<1);
+		PORTD |= ((VH & 0x40)<<1) + (VH & 0x10) + ((VH & 0x08)>>3) + ((VH & 0x04)>>1) + ((VH & 0x03)<<2) + ((VL & 0x10)<<2);
+		PORTE |= ((VH & 0x80)>>1);
+		PORTF |= ((VL & 0x80)>>1) + ((VL & 0x40)<<1);
+
 		pulse_low(P_WR, B_WR);
 		break;
 	case LATCHED_16:
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-		PORTG &= ~0x20;	
-		PORTG |= (VH & 0x10)<<1;	
-		PORTH &= ~0x18;	
-		PORTH |= (VH & 0xC0)>>3;	
-		PORTE &= ~0x3B;	
-		PORTE |= (VH & 0x03) + ((VH & 0x0C)<<2) + ((VH & 0x20)>>2);
+		cport(PORTC, 0xBF);
+		cport(PORTD, 0x60);
+		cport(PORTE, 0xBF);
+		PORTC += ((VH & 0x20)<<1);
+		PORTD += ((VH & 0x40)<<1) + (VH & 0x10) + ((VH & 0x08)>>3) + ((VH & 0x04)>>1) + ((VH & 0x03)<<2);
+		PORTE += ((VH & 0x80)>>1);
 		cbi(P_ALE, B_ALE);
 		pulse_high(P_ALE, B_ALE);
 		cbi(P_CS, B_CS);
-		PORTG &= ~0x20;	
-		PORTG |= (VL & 0x10)<<1;	
-		PORTH &= ~0x18;	
-		PORTH |= (VL & 0xC0)>>3;	
-		PORTE &= ~0x3B;	
-		PORTE |= (VL & 0x03) + ((VL & 0x0C)<<2) + ((VL & 0x20)>>2);
-#else
-		PORTD = VH;
-		cbi(P_ALE, B_ALE);
-		pulse_high(P_ALE, B_ALE);
-		cbi(P_CS, B_CS);
-		PORTD =  VL;
-#endif
+		cport(PORTC, 0xBF);
+		cport(PORTD, 0x60);
+		cport(PORTE, 0xBF);
+		PORTC += ((VL & 0x20)<<1);
+		PORTD += ((VL & 0x40)<<1) + (VL & 0x10) + ((VL & 0x08)>>3) + ((VL & 0x04)>>1) + ((VL & 0x03)<<2);
+		PORTE += ((VL & 0x80)>>1);
 		pulse_low(P_WR, B_WR);
 		sbi(P_CS, B_CS);
 		break;
@@ -118,43 +121,39 @@ void UTFT::LCD_Writ_Bus(char VH,char VL, byte mode)
 
 void UTFT::_set_direction_registers(byte mode)
 {
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	if (mode!=LATCHED_16)
+	switch (mode)
 	{
-		DDRA = 0xFF;
-		if (mode==16)
-			DDRC = 0xFF;
+	case 8:
+	case LATCHED_16:
+		DDRC |= 0x40;
+		DDRD |= 0x9F;
+		DDRE |= 0x40;
+		break;
+	case 16:
+		DDRB |= 0xF0;
+		DDRC |= 0xC0;
+		DDRD |= 0xDF;
+		DDRE |= 0x40;
+		DDRF |= 0xC0;
+		break;
 	}
-	else
-	{
-		DDRH = 0x18;
-		DDRG = 0x20;
-		DDRE = 0x3B;
-	}
-#else
-	DDRD = 0xFF;
-	if (mode==16)
-	{
-		DDRB |= 0x3F;
-		DDRC |= 0x03;
-	}
-#endif
-
 }
 
 void UTFT::_fast_fill_16(int ch, int cl, long pix)
 {
 	long blocks;
 
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	PORTA = ch;
-	PORTC = cl;
-#else
-	PORTD = ch;
-	cport(PORTC, 0xFC);
-	sport(PORTC, (cl>>6) & 0x03);
-	PORTB =  cl & 0x3F;
-#endif
+	cport(PORTB, 0x0F);
+	cport(PORTC, 0x3F);
+	cport(PORTD, 0x20);
+	cport(PORTE, 0xBF);
+	cport(PORTF, 0x3F);
+
+	PORTB |= ((cl & 0x0F)<<4);
+	PORTC |= ((cl & 0x20)<<2) + ((ch & 0x20)<<1);
+	PORTD |= ((ch & 0x40)<<1) + (ch & 0x10) + ((ch & 0x08)>>3) + ((ch & 0x04)>>1) + ((ch & 0x03)<<2) + ((cl & 0x10)<<2);
+	PORTE |= ((ch & 0x80)>>1);
+	PORTF |= ((cl & 0x80)>>1) + ((cl & 0x40)<<1);
 
 	blocks = pix/16;
 	for (int i=0; i<blocks; i++)
@@ -187,11 +186,13 @@ void UTFT::_fast_fill_8(int ch, long pix)
 {
 	long blocks;
 
-#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
-	PORTA = ch;
-#else
-	PORTD = ch;
-#endif
+	cport(PORTC, 0xBF);
+	cport(PORTD, 0x60);
+	cport(PORTE, 0xBF);
+
+	PORTC |= ((ch & 0x20)<<1);
+	PORTD |= ((ch & 0x40)<<1) + (ch & 0x10) + ((ch & 0x08)>>3) + ((ch & 0x04)>>1) + ((ch & 0x03)<<2);
+	PORTE |= ((ch & 0x80)>>1);
 
 	blocks = pix/16;
 	for (int i=0; i<blocks; i++)
